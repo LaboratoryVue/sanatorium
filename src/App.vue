acca2fd81a7ba9e5e8dda2542a85b4c5975dd439
gearmobile <gearmobile@gmail.com>
2018-06-06 12:42:23 +0300

    initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="col">
+        <StartPage msg="Welcome to Your Vue.js App"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import StartPage from './components/StartPage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    StartPage
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/StartPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="start">
+    <!--  -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  //
+</style>
